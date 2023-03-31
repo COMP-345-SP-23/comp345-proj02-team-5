f2ebc6aac2b349ee2a27a04295f0ef5011aab616
vconrad02 <99766657+vconrad02@users.noreply.github.com>
2023-03-31 10:13:02 -0400

    Started setting up LibrarianTest class

---
+++ src/test/java/LibrarianTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class LibrarianTest {
+
+    @Test
+    void addBookTest(){
+
+    }
+
+    @Test
+    void removeBookTest(){
+
+    }
+
+    @Test
+    void freezeAccountTest(){
+
+    }
+
+    @Test
+    void unfreezeAccountTest(){
+
+    }
+
+    @Test
+    void removeAccountTest(){
+
+    }
+
+    @Test
+    void getCheckedOutListTest(){
+
+    }
+
+}
