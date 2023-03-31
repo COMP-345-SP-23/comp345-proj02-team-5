d2ac652b708943bccba1a65db92b21203e4d63e9
vconrad02 <99766657+vconrad02@users.noreply.github.com>
2023-03-31 09:00:51 -0400

    Set up Librarian class

---
+++ src/main/java/Librarian.java
@@ -0,0 +1,58 @@
+public class Librarian {
+
+    private int id;
+
+    public Librarian(int id){
+        this.id = id;
+    }
+
+    /**
+     * Adds a book to the list of books at the library, or increases the number of copies
+     * if the book is already at the library
+     * return: True if successful, false if unsuccessful
+     */
+//    public boolean addBook(){
+//      used on type book
+//    }
+
+    /**
+     * Decreases the count of a book at the library
+     * return: True if successful, false if unsuccessful
+     */
+//    public boolean removeBook(){
+//      used on type book
+//    }
+
+    /**
+     * Freezes an account
+     * return: True if successful, false if unsuccessful
+     */
+//    public boolean freezeAccount(){
+//      used on type user
+//    }
+
+    /**
+     * Unfreezes an account
+     * return: True if successful, false if unsuccessful
+     */
+//    public boolean unfreezeAccount(){
+//      used on type user
+//    }
+
+    /**
+     * Removes an account from the system
+     * return: True if successful, false if unsuccessful
+     */
+//    public boolean removeAccount(){
+//      used on type user
+//    }
+
+    /**
+     * Gets a list of books currently checked out
+     * return: checkedOutList
+     */
+//    public List<Book> getCheckedOutList(){
+//      used on type library
+//    }
+
+}
