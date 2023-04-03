9ffad690983fbb51d4a59e6568d8b75ed3496485
vconrad02 <99766657+vconrad02@users.noreply.github.com>
2023-04-02 23:01:57 -0400

    Worked on Librarian

--- src/main/java/Librarian.java
+++ src/main/java/Librarian.java
@@ -1,34 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Librarian {
 
     private int id;
+    private String title;
+    private ArrayList<Book> bookList = new ArrayList<Book>();
 
     public Librarian(int id){
         this.id = id;
     }
 
+
     /**
      * Adds a book to the list of books at the library, or increases the number of copies
      * if the book is already at the library
-     * return: True if successful, false if unsuccessful
      */
-//    public boolean addBook(){
-//      used on type book
-//    }
+    public void addBook(Book book){
+//      used on type library
+        int i;
+        boolean there = false;
+        for(i = 0; i < bookList.size(); i++){
+            if(bookList.get(i) == book){
+                book.copies++;
+                there = true;
+            }
+        }
+        //make sure if not
+        if(!there){
+            bookList.add(book);
+        }
+    }
 
     /**
      * Decreases the count of a book at the library
-     * return: True if successful, false if unsuccessful
      */
-//    public boolean removeBook(){
-//      used on type book
-//    }
+    public void removeBook(Book book){
+//      used on type library
+        int i;
+        boolean there = false;
+        for(i = 0; i < bookList.size(); i++){
+            if(bookList.get(i) == book){
+                book.copies--;
+                there = true;
+            }
+        }
+        if(!there){
+            bookList.remove(book);
+        }
+    }
 
     /**
      * Freezes an account
      * return: True if successful, false if unsuccessful
      */
 //    public boolean freezeAccount(){
-//      used on type user
+//      used on type user? or library?
 //    }
 
     /**
@@ -36,7 +63,7 @@ public class Librarian {
      * return: True if successful, false if unsuccessful
      */
 //    public boolean unfreezeAccount(){
-//      used on type user
+//      used on type user? or library?
 //    }
 
     /**
@@ -49,10 +76,25 @@ public class Librarian {
 
     /**
      * Gets a list of books currently checked out
-     * return: checkedOutList
+     * return: list of books checked out
      */
 //    public List<Book> getCheckedOutList(){
 //      used on type library
 //    }
 
+    /**
+     * Gets a list of books the library has
+     * return: list of books at the library
+     */
+//    public List<Book> getBookList(){
+//        used on type library
+//    }
+
+  /**
+     * Gets a list of books the library has
+     * return: list of books at the library
+     */
+    public List<Book> getBookList(){
+        return bookList;
+    }
 }
