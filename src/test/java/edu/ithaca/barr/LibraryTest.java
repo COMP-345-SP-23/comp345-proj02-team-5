45034944bcbed26154f7d18570af9321e40d37c6
cioffig527 <giovannicioffi6180@gmail.com>
2023-04-04 05:33:26 -0400

    started TDD

---
+++ src/test/java/edu/ithaca/barr/LibraryTest.java
@@ -0,0 +1,36 @@
+package edu.ithaca.barr;
+
+import org.junit.jupiter.api.Test;
+
+public class LibraryTest {
+    
+    @Test
+    void createAccountTest(){
+        //create users and passwords, assert they are in the system and have correct values
+    }
+
+    @Test
+    void closeAccountTest(){
+        //close out accounts using usernames and passwords and assure they are removed.
+    }
+
+    @Test
+    void searchByTitleTest(){
+        
+    }
+
+    @Test
+    void searchByAuthorTest(){
+        
+    }
+
+    @Test
+    void checkOutBookTest(){
+        
+    }
+
+    @Test
+    void returnTest(){
+        
+    }
+}
