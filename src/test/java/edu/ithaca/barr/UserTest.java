7e50226013221ecd7c7f0faf8d32a3f6753cb283
vconrad02 <99766657+vconrad02@users.noreply.github.com>
2023-04-21 11:45:35 -0400

    Worked on getReservedListTest

--- src/test/java/edu/ithaca/barr/UserTest.java
+++ src/test/java/edu/ithaca/barr/UserTest.java
@@ -56,7 +56,39 @@ public class UserTest {
         assertEquals(checkedOut, user2.getCheckedOutList()); //Test books are checked out only for the specific user
 
         assertThrows(IllegalArgumentException.class, ()->   barr.checkOutBook(user2, book1));   //Test list does not change if user tries to check out a book with no copies left
+    }
 
+    @Test
 
+    public void getReservedListTest(){
+        Library barr = new Library();
+        User user1 = new User(1, "John", "abc", "123");
+        User user2 = new User(2, "Barr", "def", "456"); //Tests when there are two users
+
+        Book book1 = new Book(1, "Title 1", "Author 1", 1);
+        Book book2 = new Book(2, "Title 2", "Author 2", 0);
+        Book book3 = new Book(3, "Title 3", "Author 1", 0);
+        List<Book> reserved = new ArrayList<>();
+
+        barr.allBooks.add(book1);
+        barr.allBooks.add(book2);
+        barr.allBooks.add(book3);
+
+        assertEquals(reserved, user1.getCheckedOutList()); //Test list is empty when no books reserved
+
+        barr.checkOutBook(user1, book1);
+        assertEquals(reserved, user1.getReservedList()); //Test list is empty when a book was checked out
+
+//        barr.checkOutBook(user1, book2);
+        //Should reserve be a part of the checkOutBook method?
+//        barr.reserve(user1, book2);
+        assertEquals(reserved, user1.getCheckedOutList()); //Test list one book when a book is reserved
+
+        barr.checkOutBook(user2, book1);
+        //        barr.reserve(user2, book1);
+
+        assertEquals(reserved, user2.getCheckedOutList()); //Test books are reserved only for the specific user
+
+//        assertThrows(IllegalArgumentException.class, ()->   barr.reserved(user2, book1));   //Test list does not change if user tries to check out a book with no copies left
     }
 }
