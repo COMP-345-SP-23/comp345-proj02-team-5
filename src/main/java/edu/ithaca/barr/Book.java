f8492f9b28915ff68b1257831edf7f677d48f38c
Vanessa <vmpofu@ithaca.edu>
2023-04-22 19:22:43 -0400

    made some changes in the book class

--- src/main/java/edu/ithaca/barr/Book.java
+++ src/main/java/edu/ithaca/barr/Book.java
@@ -6,12 +6,14 @@ public class Book {
     private String title;
     private String author;
     public int numCopies;
+    public String status;
     public int durationRentalDays = 21;
 
-    public Book(int id, String title, String author, int numCopies){
+    public Book(int id, String title, String author, int numCopies, String status){
         this.id=id;
         this.title=title;
         this.author=author;
+        this.status = status;
         this.numCopies=numCopies;
     }
 
@@ -31,6 +33,9 @@ public class Book {
         return author;
     }
 
+    public String getStatus(){
+        return status;
+    }
     public String returnInfo(){
         return "ID: "+id+", Title: "+title+", Author: "+author+", Number of Copies: "+numCopies;
     }
@@ -39,5 +44,9 @@ public class Book {
         int currNumCopies = book.getNumCopies() - 1;
         numCopies = currNumCopies;
         return currNumCopies;
+    }
+
+    public void setStatus(String newStatus){
+        this.status  = newStatus;  
     }
 }
