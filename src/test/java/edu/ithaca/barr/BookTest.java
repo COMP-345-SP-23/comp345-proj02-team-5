a245144e54bf7c969bdedacbe7b3ecb15021096e
vconrad02 <99766657+vconrad02@users.noreply.github.com>
2023-05-08 16:52:23 -0400

    Added some comments

--- src/test/java/edu/ithaca/barr/BookTest.java
+++ src/test/java/edu/ithaca/barr/BookTest.java
@@ -17,9 +17,9 @@ public class BookTest {
 
         List<Book> jbooks = JsonUtil.listFromJsonFile("src/test/java/edu/ithaca/barr/books.json", Book.class);
 
-        assertEquals("ID: 0, Title: A, Author: B, Number of Copies: 0", jbooks.get(0).returnInfo());
-        assertEquals("ID: 1, Title: C, Author: D, Number of Copies: 1", jbooks.get(1).returnInfo());
-        assertEquals("ID: 2, Title: E, Author: F, Number of Copies: 5", jbooks.get(2).returnInfo());
+        assertEquals("ID: 0, Title: A, Author: B, Number of Copies: 0", jbooks.get(0).returnInfo());    //Tests that the information for a specific book is printed out
+        assertEquals("ID: 1, Title: C, Author: D, Number of Copies: 1", jbooks.get(1).returnInfo());    //Tests that the information for a different specific book is printed out
+        assertEquals("ID: 2, Title: E, Author: F, Number of Copies: 5", jbooks.get(2).returnInfo());    //Tests that the information for another specific book is printed out
 
     }
 }
